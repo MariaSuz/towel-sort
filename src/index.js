eba66331657402ee6f660fc092848a8fb4dc684f
Мария Суздальцева <suzdaltseva.mo@opera-samara.net>
2024-08-05 17:07:04 +0400

    docs: upload file

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if(matrix == null) {
+    return [];
+  }
+
+    // длинная запись
+  // let newmatrix = matrix.map(function (element,index) {
+  //   if (index %2 === 0) {
+  //     return element
+  //   }
+  //   return element.reverse()
+  // })
+
+//короткая запись
+  let newmatrix = matrix.map((element,index) => (index %2 === 0) ? element : element.reverse())
+
+
+
+  return newmatrix.flat();
 }
